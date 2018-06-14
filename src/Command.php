678a20e8d3b11f06d53717e6b1b2f23eda2a9928
James Cook <jamescook@gmlconsulting.co.uk>
2018-06-14 10:06:56 +0100

    adds base commander class

---
+++ src/Command.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Pangolinkeys\Commander;
+
+class Command
+{
+    protected $commands = [];
+
+    /**
+     * Method to call a command by key.
+     *
+     * @param $name
+     * @return null
+     */
+    public function call($name)
+    {
+        if (array_has($this->commands, $name)) {
+            return $this->commands[$name]();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Internal method to register a command.
+     *
+     * @param          $name
+     * @param callable $closure
+     */
+    protected function register($name, callable $closure)
+    {
+        $this->commands[] = [
+            $name => $closure,
+        ];
+    }
+}
